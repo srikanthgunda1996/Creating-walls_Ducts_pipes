b40e0f0f8a31eb4c5172d6a1d7b5a2c80e9d169e
srikanthgunda1996 <srigunda789@gmail.com>
2023-08-06 00:50:13 +0530

    Updated

--- Creating walls_Ducts_pipes/ModuleChallenge2.cs
+++ Creating walls_Ducts_pipes/ModuleChallenge2.cs
@@ -7,6 +7,8 @@ using Autodesk.Revit.DB.Plumbing;
 using Autodesk.Revit.UI;
 using Autodesk.Revit.UI.Selection;
 using System;
+using System.Windows.Forms;
+using System.Drawing;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Reflection;
@@ -46,6 +48,28 @@ namespace Creating_walls_Ducts_pipes
             }
             return PipingSystem;
         }
+        internal Parameter GetParameterByName(Element Elem, string paramName)
+        {
+            foreach (Parameter Param in Elem.Parameters)
+            {
+                if (Param.Definition.Name.ToString() == paramName)
+                    return Param;
+            }
+
+            return null;
+        }
+        internal bool SetParameterValue(Element curElem, string paramName, double value)
+        {
+            Parameter curParam = GetParameterByName(curElem, paramName);
+            if (curParam != null)
+            {
+                curParam.Set(value);
+                return true;
+            }
+
+            return false;
+
+        }
 
         public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
         {
@@ -75,13 +99,15 @@ namespace Creating_walls_Ducts_pipes
             Transaction t = new Transaction(doc);
             t.Start("create walls");
             string ductsystyp = "";
+            string pipesystyp = "";
+
             TaskDialog tdg = new TaskDialog("Duct Systems");
             tdg.MainInstruction = "Select Duct System:";
             tdg.AddCommandLink(TaskDialogCommandLinkId.CommandLink1, "Return Air");
             tdg.AddCommandLink(TaskDialogCommandLinkId.CommandLink2, "Exhaust Air");
             tdg.AddCommandLink(TaskDialogCommandLinkId.CommandLink3, "Supply Air");
             TaskDialogResult selectedsystem = tdg.Show();
-            if(selectedsystem == TaskDialogResult.CommandLink1)
+            if (selectedsystem == TaskDialogResult.CommandLink1)
             {
                 ductsystyp = "Return Air";
             }
@@ -93,11 +119,32 @@ namespace Creating_walls_Ducts_pipes
             {
                 ductsystyp = "Supply Air";
             }
+
+            TaskDialog tsdg = new TaskDialog("Pipe Systems");
+            tsdg.MainInstruction = "Select Pipe System:";
+            tsdg.AddCommandLink(TaskDialogCommandLinkId.CommandLink1, "Domestic Cold Water");
+            tsdg.AddCommandLink(TaskDialogCommandLinkId.CommandLink2, "Fire Protection Dry");
+            tsdg.AddCommandLink(TaskDialogCommandLinkId.CommandLink3, "Hydronic Supply");
+            TaskDialogResult selectedpipesystem = tsdg.Show();
+            if (selectedpipesystem == TaskDialogResult.CommandLink1)
+            {
+                pipesystyp = "Domestic Cold Water";
+            }
+            if (selectedpipesystem == TaskDialogResult.CommandLink2)
+            {
+                pipesystyp = "Fire Protection Dry";
+            }
+            if (selectedpipesystem == TaskDialogResult.CommandLink3)
+            {
+                pipesystyp = "Hydronic Supply";
+            }
+
+
             FilteredElementCollector ducttype = new FilteredElementCollector(doc).OfClass(typeof (DuctType));
             FilteredElementCollector walltype = new FilteredElementCollector(doc).OfClass(typeof(WallType));
             FilteredElementCollector pipetype = new FilteredElementCollector(doc).OfClass(typeof(PipeType));
             MEPSystemType ductsystemtype = getductsystemtype(doc, ductsystyp);
-            PipingSystemType pipesystemtype = getpipesystemtype(doc, "Domestic Cold Water");
+            PipingSystemType pipesystemtype = getpipesystemtype(doc, pipesystyp);
             WallType storefrontwall = null;
             WallType genwall = null;
            foreach(WallType genwall1 in walltype)
@@ -162,9 +209,63 @@ namespace Creating_walls_Ducts_pipes
                 }
 
             }
+            FilteredElementCollector dct = new FilteredElementCollector(doc).OfClass(typeof(Duct));
+            int wid = 0;
+            int hgt = 0;
+            foreach (Element currentduct in dct)
+            { 
+                    TaskDialog tsdg2 = new TaskDialog("Duct Size");
+                    tsdg2.MainInstruction = "Select Duct Size:";
+                    tsdg2.AddCommandLink(TaskDialogCommandLinkId.CommandLink1, @"1'x1'");
+                    tsdg2.AddCommandLink(TaskDialogCommandLinkId.CommandLink2, @"2'x2'");
+                    tsdg2.AddCommandLink(TaskDialogCommandLinkId.CommandLink3, @"3'x3'");
+                    TaskDialogResult DuctSize = tsdg2.Show();
+                    if (DuctSize == TaskDialogResult.CommandLink1)
+                    {
+                        wid = 1;
+                        hgt = 1;
+                    }
+                    if (DuctSize == TaskDialogResult.CommandLink2)
+                    {
+                        wid = 2;
+                        hgt = 2;
+                }
+                    if (DuctSize == TaskDialogResult.CommandLink3)
+                    {
+                        wid = 3;
+                        hgt = 3;
+                }
+                bool ductwidth = SetParameterValue(currentduct, "Width", wid);
+                bool ductheight = SetParameterValue(currentduct, "Height", hgt);
+            }
+            FilteredElementCollector pip = new FilteredElementCollector(doc).OfClass(typeof(Pipe));
+            int dia = 0;
+            foreach (Element currentpipe in pip)
+            {
+                TaskDialog tsdg3 = new TaskDialog("Pipe Size");
+                tsdg3.MainInstruction = "Select Pipe Size:";
+                tsdg3.AddCommandLink(TaskDialogCommandLinkId.CommandLink1, @"1'");
+                tsdg3.AddCommandLink(TaskDialogCommandLinkId.CommandLink2, @"2'");
+                tsdg3.AddCommandLink(TaskDialogCommandLinkId.CommandLink3, @"3'");
+                TaskDialogResult PipeSize = tsdg3.Show();
+                if (PipeSize == TaskDialogResult.CommandLink1)
+                {
+                    dia = 1;
+                }
+                if (PipeSize == TaskDialogResult.CommandLink2)
+                {
+                    dia = 2;
+                }
+                if (PipeSize == TaskDialogResult.CommandLink3)
+                {
+                    dia = 3;
+                }
+                bool pipedia = SetParameterValue(currentpipe, "Diameter", dia);
+            }
+
+
             t.Commit();
             t.Dispose();
-            TaskDialog.Show("Model Curves selected", count.ToString() + " " + "modelcurves got selected");
             return Result.Succeeded;
         }
 
